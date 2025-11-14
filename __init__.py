31427059f8e4fd69692a128557faf23fba625011
Mann Malviya <mannmalviya15@gmail.com>
2025-11-14 13:59:03 -0800

    main and run files added

---
+++ __init__.py
@@ -0,0 +1,6 @@
+"""
+    Python needs `__init__.py` files because so that it recognizes this folder (/CodeCompass-Refactored) as a importable packages
+    - So Relative imports work
+    - & so that python3 -m yourpackage.run works
+    - These can be left empty for the most part
+"""
--- main.py
+++ main.py
@@ -9,9 +9,31 @@
 
 """
 
+import uvicorn
 from fastapi import FastAPI
 
 # import the routers
-from api.routes_chat
+from .api import routers_chat
+from .api import routers_documentation
+from .api import routers_indexing
 
+
+
+app = FastAPI()
+
+app.include_router(routers_chat.router)
+app.include_router(routers_indexing.router)
+app.include_router(routers_documentation.router)
+
+# === Setting Up the Ollama Server ====
+import subprocess   # allows running system commands(like starting the Ollama Server)
+import time     # provides time related funcs like sleep()
+import socket   # used for network communication, in our case to ches if Ollama server is running
+
+def is_ollama_running(host="127.0.0.1", port=11434):
+    try:
+        with socket.create_connection((host, port), timeout=1):
+            return True
+    except OSError:
+        return False
 
