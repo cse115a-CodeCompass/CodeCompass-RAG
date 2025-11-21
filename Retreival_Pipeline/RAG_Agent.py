61406fe24b795a68d1bd5cb9454ba2d2986d59ed
Mann Malviya <mannmalviya@gmail.com>
2025-11-20 16:11:02 -0800

    added Ollama & Models Manager

--- Retreival_Pipeline/RAG_Agent.py
+++ Retreival_Pipeline/RAG_Agent.py
@@ -12,16 +12,89 @@
     - It receives `query`, `chat_history` & ``
     
     - An “orchestrator agent” in a RAG system is usually just a normal LLM that has tool-calling abilities.
-
-
-
-    
-
-
 """
 
-class Agent:
+import ollama
+
+
+
+class RAG_Agent:
+    """
+        An orchestrator agent for selecting the appropriate retrieval tool
+        based on the user's query. The agent decides between:
+
+        1. Codebase Documentation RAG Pipeline
+        2. Code RAG Pipeline
+        3. Keyword Search
+        4. No retreival necessary
+
+        The agent:
+        - Receives a natural language query
+        - Uses an LLM classifier(lightweight LLM)
+        - Executes the chosen retrieval pipeline
+        - Prompts the selected LLM with the assembled response context & query
+        - Streams the final LLM response back to the user
+    """
     def __init__(self):
+
+        self.agent_llm = "qwen2.5:3b"
+        
+
+
         pass
 
-    
+    def select_tool(self, query: str):
+        """
+            LLM Decides which Knowledge Source to retrieve from, for given user query.
+
+            Args:
+                query (str): Entered User Query
+
+            Returns one of:
+                - "docs_rag"
+                - "code_rag"
+                - "keyword_search"
+                - "conversation_history"
+        """
+
+        
+
+
+        return
+
+    def dispatch_to_tool(self, tool_name, query, chat_history):
+        """
+            Executes the selected tool & returns the retreived context.
+
+            Args:
+                tool_name (str):
+                query (str): Entered User Query
+
+            Returns:
+                Retrieved context (str)
+        """
+
+        return
+
+
+    def query_llm():
+        """
+        """
+        return
+
+    def run(self):
+        """
+        """
+
+        return
+
+
+
+
+# This main() is only for testing purposes
+def main():
+    return
+
+if __name__ == "__main__":
+    main()
+
